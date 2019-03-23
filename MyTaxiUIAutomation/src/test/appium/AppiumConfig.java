069a242c719ed51c862a28ff640cd9405bc15ae9
dtadin <dtadin@Domagojs-MacBook-Pro.local>
2019-03-23 19:59:22 -0400

    setting up dependencies for appium and espresso

---
+++ MyTaxiUIAutomation/src/test/appium/AppiumConfig.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.remote.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppiumConfig {
+
+    public void setUp(){
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("deviceName", "<device id>");
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
+        capabilities.setCapability(CapabilityType.VERSION, "<android version>");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("appPackage", "com.google.android.calculator");
+        capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+    }
+}
