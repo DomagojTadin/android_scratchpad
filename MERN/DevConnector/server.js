2e38ecbafe3aeac1bcffc513efeecfb25d31c232
dtadin <dtadin@Domagojs-MacBook-Pro.local>
2019-06-22 15:13:06 -0400

    fixed submodule issue with git

---
+++ MERN/DevConnector/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const connectdb = require("./config/db");
+const app = express();
+
+connectdb();
+
+app.get("/", (req, res) => res.send("API running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
