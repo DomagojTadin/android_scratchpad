a598245cbd76f3198fe78ecc6de6e33b733384b7
dtadin <dtadin@Domagojs-MacBook-Pro.local>
2019-06-22 15:32:46 -0400

    finished devconnector test routes

--- MERN/DevConnector/server.js
+++ MERN/DevConnector/server.js
@@ -6,6 +6,12 @@ connectdb();
 
 app.get("/", (req, res) => res.send("API running"));
 
+// define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/authorization", require("./routes/api/authorization"));
+app.use("/api/profiles", require("./routes/api/profiles"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
