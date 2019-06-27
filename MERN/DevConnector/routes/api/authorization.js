e4273cd41b199158bd15ece68709abf6185295da
dtadin <dtadin@Domagojs-MacBook-Pro.local>
2019-06-27 18:45:46 -0400

    moved mern stack projects to their own repo

--- MERN/DevConnector/routes/api/authorization.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// @route  api/auth
-// @desc   fetch user information
-// @access public
-router.get("/", (req, res) => res.send("auth route"));
-
-module.exports = router;
